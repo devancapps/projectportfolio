46597d7237b4f56c05f88ec63eb5f27d4ebdc18a
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-12-09 15:07:10 -0600

    About me

---
+++ src/components/AboutMe.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+function AboutMe() {
+  return (
+    <section>
+      <h2>About Me</h2>
+      <img src="" alt="Devan Image" />
+      <p>Bio text.</p>
+    </section>
+  );
+}
+
+export default AboutMe;
