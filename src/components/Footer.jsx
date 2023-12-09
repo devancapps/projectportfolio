201e673f2d772e1e22db6c3e8abf4a4b59b1464b
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-12-09 15:11:19 -0600

    Footer Component

---
+++ src/components/Footer.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function Footer() {
+  return (
+    <footer>
+      <a href="your_github_link">GitHub</a>
+      <a href="your_linkedin_link">LinkedIn</a>
+    </footer>
+  );
+}
+
+export default Footer;
