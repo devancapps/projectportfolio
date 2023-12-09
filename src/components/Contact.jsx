4e26546316fa8e72e2c5b8508e7eb651afedadfa
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-12-09 15:09:35 -0600

    Contact component

---
+++ src/components/Contact.jsx
@@ -0,0 +1,28 @@
+import React, { useState } from 'react';
+
+function Contact() {
+  const [formData, setFormData] = useState({ name: '', email: '', message: '' });
+
+  const handleChange = (event) => {
+    setFormData({ ...formData, [event.target.name]: event.target.value });
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+   
+  };
+
+  return (
+    <section>
+      <h2>Contact</h2>
+      <form onSubmit={handleSubmit}>
+        <input type="text" name="name" placeholder="Name" onChange={handleChange} required />
+        <input type="email" name="email" placeholder="Email" onChange={handleChange} required />
+        <textarea name="message" placeholder="Message" onChange={handleChange} required></textarea>
+        <button type="submit">Submit</button>
+      </form>
+    </section>
+  );
+}
+
+export default Contact;
