66700d98365a3e01be4990f0ccc30f26d692d157
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-12-09 15:26:56 -0600

    Bio updated

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -5,7 +5,7 @@ function AboutMe() {
     <section>
       <h2>About Me</h2>
       <img src="" alt="Devan Image" />
-      <p>Bio text.</p>
+      <p>As a Full Stack Developer with a knack for digital marketing, I specialize in creating engaging, effective landing pages and lead generation forms. My skills in front-end and back-end development ensure smooth, intuitive web experiences. I'm adept at integrating web development with digital marketing strategies to boost online engagement. With a solid grasp of various programming languages and an eye on current digital marketing trends, I'm focused on using technology to solve real-world problems, drive business growth, and stay updated with the latest in web development..</p>
     </section>
   );
 }
