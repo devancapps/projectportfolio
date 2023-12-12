25290c09e4b9991c5219d6f2d265ca289d3536fb
DevanCapps <145359970+devancapps@users.noreply.github.com>
2023-12-12 12:33:45 -0600

    alt attribute

--- src/components/AboutMe.jsx
+++ src/components/AboutMe.jsx
@@ -4,7 +4,7 @@ function AboutMe() {
   return (
     <section>
       <h2>About Me</h2>
-      <img src="" alt="Devan Image" />
+      <img src="" alt="Devan" />
       <p>As a Full Stack Developer with a knack for digital marketing, I specialize in creating engaging, effective landing pages and lead generation forms. My skills in front-end and back-end development ensure smooth, intuitive web experiences. I'm adept at integrating web development with digital marketing strategies to boost online engagement. With a solid grasp of various programming languages and an eye on current digital marketing trends, I'm focused on using technology to solve real-world problems, drive business growth, and stay updated with the latest in web development..</p>
     </section>
   );
